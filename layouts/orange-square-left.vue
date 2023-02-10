a66cff3fec8ba0d7b4ccab10a115b2a63b002f93
ecuriotto <7021326+ecuriotto@users.noreply.github.com>
2023-02-10 14:03:48 +0200

    another try

--- layouts/orange-square-left.vue
+++ layouts/orange-square-left.vue
@@ -1,18 +1,6 @@
-<script setup lang="ts">
-import { computed } from 'vue'
-import { handleBackground } from '../layoutHelper'
-
-const props = defineProps({
-    background: {
-        default: '../public/images/left-orange-square.png',
-    },
-})
-
-const style = computed(() => handleBackground(props.background, false))
-</script>
 
 <template>
-    <div class="slidev-layout cover" :style="style">
+    <div class="slidev-layout cover orange-square-left">
         <div class="absolute top-10 right-19 text-black">
             <slot />
         </div>
